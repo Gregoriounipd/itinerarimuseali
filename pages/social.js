e9aa08853b66a540e324242c72ce800ddb088a69
Gregoriounipd <mangionegregorio28@gmail.com>
2025-06-16 15:18:13 +0200

    social.js

--- pages/social.js
+++ pages/social.js
@@ -4,10 +4,10 @@ export default function info() {
     return (
         <>
             <Head>
-                <title> Cos'é questo sito? </title>
+                <title> Cos&apos;é questo sito? </title>
             </Head>
             <main className="max-w-3x1 mx-auto p-6">
-                <h1 className="text-4x1 font-bold mb-4 text-gray-900">Cos'è questo sito?</h1>
+                <h1 className="text-4x1 font-bold mb-4 text-gray-900">Cos&apos;è questo sito?</h1>
                 <p className="text-lg text-gray-700 leading-relaxed">
                     QUalcosa di scritto, some random things
 
